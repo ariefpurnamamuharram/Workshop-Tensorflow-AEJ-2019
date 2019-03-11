263d9935d592e51eae19361e139af22df773fddd
Arief Purnama Muharram <ariefpurnamamuharram@gmail.com>
2019-03-11 23:40:20 +0700

    Update: activity_main layout

--- android/kotlin/app/src/main/java/com/workshopaej2019/imagerecognitionapplication/MainActivity.kt
+++ android/kotlin/app/src/main/java/com/workshopaej2019/imagerecognitionapplication/MainActivity.kt
@@ -1,7 +1,8 @@
 package com.workshopaej2019.imagerecognitionapplication
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,4 +10,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    override fun onStart() {
+        super.onStart()
+        camera.start()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        camera.stop()
+    }
+
 }
